2515897e1bc9f6f04728c850e2f9019efe6b2e94
kristian andersen <krilleandersen@gmail.com>
2020-03-11 12:36:32 +0100

    Add files via upload
    Small update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let fileArray=[];
 let count=0;
 
 //joining path of directory 
-const directoryPath = path.join(__dirname, 'input');
+const directoryPath = path.join(__dirname, 'inputmov');
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
     //handling error
@@ -14,15 +14,20 @@ fs.readdir(directoryPath, function (err, files) {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    fileArray=files;
+   
+    
+    fileArray=files.filter(item => item !== '.DS_Store')
+
+    console.log(fileArray)
     addPtxt()
 });
 
 
+
+
 function addPtxt(){
-
-
-const ff2=['-i', 'input/'+fileArray[count]+'', '-i', 'pxtx.png','-filter_complex', "[0:v][1:v] overlay=0:0:enable='between(t,0.5,10)'",'-pix_fmt', 'yuv420p', '-c:a', 'copy','output/'+fileArray[count]+''];
+/*    
+const ff2=['-i', 'inputmov/'+fileArray[count]+'', '-i', 'pxtx.png','-filter_complex', "[0:v][1:v] overlay=0:0:enable='between(t,12,20)'",'-pix_fmt', 'yuv420p', '-c:a', 'copy','outputmov/'+fileArray[count]+''];
 
 var ffmpeg = spawn('ffmpeg', ff2);
             // input_file.pipe(ffmpeg.stdin);
@@ -52,6 +57,44 @@ var ffmpeg = spawn('ffmpeg', ff2);
                 }
                
             });
-            
+           
           
+}
+*/
+
+
+
+    
+let input ='inputmov/'+fileArray[count]
+ let movlen= ['-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1','-hide_banner', input,'-print_format','json'];
+
+ //['-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1','-hide_banner', input];
+ //['-i',input,'-hide_banner']
+
+ 
+ var ffmpeg1 = spawn('ffprobe', movlen)
+ //console.log(ffmpeg1)
+ ffmpeg1.stderr.on('data', function (data) {
+    console.log(data.toString());
+});
+
+ffmpeg1.stderr.on('end', function () {
+   // console.log('file has been converted succesfully');
+});
+
+ffmpeg1.stderr.on('exit', function () {
+   // console.log('child process exited');
+});
+
+ffmpeg1.stderr.on('close', function() {
+    if(count==fileArray.length-1){
+        console.log('...closing time! bye');
+    }else{
+        console.log('...wuub wuub onemore time');
+        count++
+        
+        addPtxt()
+    }
+}) 
+
 }
